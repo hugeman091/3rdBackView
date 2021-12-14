1d71b5f6eae26cbb6b84b9e2f8851d45db4e4021
개발자윤태웅태윤 <84231954+hugeman091@users.noreply.github.com>
2021-12-14 15:19:16 +0900

    Add files via upload
    Cinemachine을 이용한 3인칭 백뷰

---
+++ LocomotionController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Animator))]
+public class LocomotionController : MonoBehaviour
+{
+	[SerializeField] Animator m_Animator;
+	[SerializeField] MoveController m_Mover;
+	[SerializeField] AimViewController m_AimView;
+
+	public InputAction fireAction;
+	private void Start()
+	{
+		if (!m_Animator)
+			m_Animator = GetComponent<Animator>();
+		if (!m_Mover)
+			m_Mover = GetComponent<MoveController>();
+		if (!m_AimView)
+			m_AimView = GetComponent<AimViewController>();
+	}
+
+	void OnEnable()
+	{
+		fireAction.Enable();
+	}
+
+	void OnDisable()
+	{
+		fireAction.Disable();
+	}
+
+	private void Update()
+	{
+		RunAnimation();
+		AttackAnimation();
+	}
+
+	void RunAnimation()
+	{
+		if (!m_Mover)
+			return;
+
+		m_Animator.SetBool("IsMoving", m_Mover.m_IsMoving);
+		m_Animator.SetFloat("VelocityY", m_Mover.m_MoveSpeed);
+	}
+
+	void AttackAnimation()
+	{
+		if (!m_AimView)
+			return;
+
+		m_Animator.SetBool("IsAiming", m_AimView.m_IsAiming);
+		m_Animator.SetBool("Fire", fireAction.triggered);
+	}
+	private void OnAnimatorMove()
+	{
+		//좌표이동 애니메이션이 있으면 여기서 사용
+	}
+}
---
+++ MoveController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class MoveController : MonoBehaviour
+{
+
+	public float m_RotateSpeed = 10f;
+	public float m_MoveSpeed = 3f;
+	public NavMeshAgent m_NavmeshAgent;
+	public bool m_IsMoving;
+
+	private Vector3 m_Dest;
+	private Vector3 m_Dir;
+
+	private void Start()
+	{
+		if (!m_NavmeshAgent)
+			m_NavmeshAgent = GetComponent<NavMeshAgent>();
+	}
+	private void FixedUpdate()
+	{
+		Move();
+	}
+	private void Move()
+	{
+		m_NavmeshAgent.nextPosition = m_Dest;
+	}
+
+
+	public void MoveTo(Vector3 dir, bool move)
+	{
+		m_IsMoving = move;
+
+		if (!m_IsMoving)
+			return;
+
+		m_Dir = dir;
+		m_Dest = transform.position + dir * m_MoveSpeed * Time.deltaTime;
+
+		//constant speed 내고싶어서 강제로 세팅
+		m_NavmeshAgent.acceleration = 1000;
+		m_NavmeshAgent.speed = m_MoveSpeed;
+		m_NavmeshAgent.angularSpeed = 360;
+	}
+}
