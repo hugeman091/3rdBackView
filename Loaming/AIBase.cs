27d5186d24eabd0791831c1996e8c26da5f38260
개발자윤태웅태윤 <84231954+hugeman091@users.noreply.github.com>
2021-12-14 18:40:20 +0900

    Add files via upload

---
+++ Loaming/AIBase.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+class AIBase : MonoBehaviour
+{
+	[SerializeField] private MoveController m_Mover;
+	[SerializeField] private PatrolPath m_PatrolPath;
+
+	[SerializeField] float m_WaypointTolerance = 0.1f;
+	[SerializeField] float m_WaypointDwellTime = 3f;
+
+	int m_CurrentWaypointIndex = 0;
+	float m_TimeSinceArraivedAtWaypoint = Mathf.Infinity;
+
+	void Start()
+	{
+		if (!m_Mover)
+			m_Mover = GetComponent<MoveController>();
+	}
+
+	void Update()
+	{
+		m_TimeSinceArraivedAtWaypoint += Time.deltaTime;
+
+		Loaming();
+	}
+
+	private void Loaming()
+	{
+		if (m_PatrolPath == null)
+			return;
+
+		if (reachedWaypoint())
+		{
+			m_CurrentWaypointIndex = m_PatrolPath.GetNextIndex(m_CurrentWaypointIndex);
+			m_Mover.m_IsMoving = false;
+
+			m_TimeSinceArraivedAtWaypoint = 0;
+		}
+		else if (m_TimeSinceArraivedAtWaypoint > m_WaypointDwellTime)
+		{
+			m_Mover.MoveTo(m_PatrolPath.GetWaypoint(m_CurrentWaypointIndex));
+			m_Mover.m_IsMoving = true;
+		}
+	}
+
+	private Vector3 GetCurrentWaypoint()
+	{
+		return m_PatrolPath.GetWaypoint(m_CurrentWaypointIndex);
+	}
+	private bool reachedWaypoint()
+	{
+		float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+		return distanceToWaypoint < m_WaypointTolerance;
+	}
+
+}
---
+++ Loaming/PatrolPath.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+class PatrolPath : MonoBehaviour
+{
+	void OnDrawGizmos()
+	{
+		const float mWaypointGizmoRadius = 0.3f;
+
+		for (int i = 0; i < transform.childCount; ++i)
+		{
+			int j = GetNextIndex(i);
+			Gizmos.DrawSphere(GetWaypoint(i), mWaypointGizmoRadius);
+			Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
+		}
+	}
+
+	public int GetNextIndex(int i)
+	{
+		if (i + 1 == transform.childCount)
+			return 0;
+		return i + 1;
+	}
+
+	public Vector3 GetWaypoint(int i)
+	{
+		return transform.GetChild(i).position;
+	}
+}
