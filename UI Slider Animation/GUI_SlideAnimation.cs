541b1b1b6c85b56dae719957dd6e2e87b17b8fa1
개발자윤태웅태윤 <84231954+hugeman091@users.noreply.github.com>
2022-01-06 15:20:30 +0900

    Add files via upload

---
+++ UI Slider Animation/GUI_SlideAnimation.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using DG.Tweening.Core;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GUI_SlideAnimation : MonoBehaviour , IPointerClickHandler
+{
+	public Slider _Slider;
+
+	void Awake() => DOTween.Init(true);
+
+
+	void IncreaseValue()
+	{
+		DOTween.To(
+				() => _Slider.value,
+				x => _Slider.value = x, 0.7f, 2f)
+			.SetEase(Ease.InOutCubic);
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (eventData.clickCount == 1)
+		{
+			IncreaseValue();
+		}
+	}
+}
